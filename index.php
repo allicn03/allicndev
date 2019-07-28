566262982ec887a9e3fbaa1090762a7a2512806f
allicn03 <a.neusch@gmail.com>
2019-07-28 10:32:32 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: allicndev';
+    $to = 'a.neusch@gmail.com';
+    $subject = 'Hello';
+    $body = "From: $name\n E-mail: $email\n Message:\n $message";
+?>
+
+<?php
+  if ($_POST['submit']) {
+    /* Anything that goes in here is only performed if the form is submitted */
+    if (mail ($to, $subject, $body, $from)) {
+        echo '<p>Your message has been sent!</p>';
+    } else {
+        echo '<p>Something went wrong, go back and try again!</p>';
+    }
+  }
+?>
